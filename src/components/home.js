66f9561da78513d09397dae17ed92596ef2e21c5
ansike <ansike@zuoyebang.com>
2019-10-27 23:21:35 +0800

    start

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: This is a description
+ * @Author: Ask
+ * @LastEditors: Ask
+ * @Date: 2019-10-27 20:46:59
+ * @LastEditTime: 2019-10-27 23:21:56
+ */
 // @flow
 import React, { Component } from 'react';
 
@@ -8,7 +15,7 @@ class Home extends Component<{}, {}> {
   render() {
     return (
       <div className="home-page">
-        <h6>Welcome to React-Wap-Aim.</h6>
+        <h6>start</h6>
       </div>
     )
   }
